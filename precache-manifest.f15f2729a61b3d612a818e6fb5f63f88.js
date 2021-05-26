ab9bc9f3c16a487718fe61178cd89fc20fab3b3a
Anurag <adanurag29@gmail.com>
2021-05-26 07:35:38 +0530

    Updates

---
+++ precache-manifest.f15f2729a61b3d612a818e6fb5f63f88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78afe977af68d6f9bd9b",
+    "url": "/addressbook-v2/static/css/main.1718cfd2.chunk.css"
+  },
+  {
+    "revision": "78afe977af68d6f9bd9b",
+    "url": "/addressbook-v2/static/js/main.79acd565.chunk.js"
+  },
+  {
+    "revision": "9cf574f0a48de412b0aa",
+    "url": "/addressbook-v2/static/js/runtime~main.cd6e0843.js"
+  },
+  {
+    "revision": "7e60b03269606a95b825",
+    "url": "/addressbook-v2/static/js/2.ba389b8d.chunk.js"
+  },
+  {
+    "revision": "f541fb5a217f1b511673afec70fed716",
+    "url": "/addressbook-v2/static/media/macrometa-white-transparent.f541fb5a.png"
+  },
+  {
+    "revision": "25ef280441ad6d3a5ccf89960b4e95eb",
+    "url": "/addressbook-v2/static/media/circle_spinner.25ef2804.gif"
+  },
+  {
+    "revision": "b69db09bb11a2b7a6c9d8befad8c4cf4",
+    "url": "/addressbook-v2/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/addressbook-v2/precache-manifest.f15f2729a61b3d612a818e6fb5f63f88.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/addressbook-v2/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
